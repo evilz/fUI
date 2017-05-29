33c6650ea703df2d5e84b0c95c536430d1a61d48
Vincent B <evilznet@gmail.com>
2017-05-29 17:42:29 +0200

    Update fUI.fs

--- src/fUI/fUI.fs
+++ src/fUI/fUI.fs
@@ -1,5 +1,32 @@
 ﻿namespace fUI
 
+
+// NEW
+
+
+module fUI =
+    type Parent = | Widget | Screen
+    type Coord = { top:int; left: int}
+    type Size = { width:int ; height: int  }
+
+    type Form = { parent:Parent; keys:bool; size:Size; position:Coord; background: string; content: string }
+
+    let form = {
+        parent = Parent.Screen;
+        keys= true;
+        size = { width = 30; height = 4 };
+        position = { left = 0 ; top = 0 };
+        background = "green";
+        content = "Submit or cancel?"
+    }
+    
+ //
+
+
+
+
+
+
 module Interface = 
     open System.Runtime.CompilerServices
     open System.IO
